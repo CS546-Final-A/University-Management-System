ae422a91faa8709abde0cd96f7d1b5519b95df93
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2023-11-01 19:16:05 -0400

    Configured lusca and ejs for use
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import express from "express";
+import lusca from "lusca";
+import ejs from "ejs";
 
 const app = express();
+
+app.use(
+	lusca({
+		csrf: true,
+		/*csp: {
+			 ... 
+		},*/
+		xframe: "SAMEORIGIN",
+		p3p: "ABCDEF",
+		hsts: { maxAge: 31536000, includeSubDomains: true, preload: true },
+		xssProtection: true,
+		nosniff: true,
+		referrerPolicy: "same-origin",
+	})
+);
+
+app.set("view engine", ejs);
 
 app.listen(8080, () => {
 	console.log("Running web server on port 8080");
