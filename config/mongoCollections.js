040ad84a9ff391792aebfcb33e41f092d8ec8d3f
Shoaib Kalawant <shoaib.sk.10.sk@gmail.com>
2023-11-03 02:08:13 -0400

    Basic Mongo Config

---
+++ config/mongoCollections.js
@@ -0,0 +1,16 @@
+import {dbConnection} from './mongoConnection.js';
+
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+/* Now, you can list your collections here: */
---
+++ config/settings.js
@@ -0,0 +1,4 @@
+export const mongoConfig = {
+    serverUrl: 'mongodb://localhost:27017/',
+    database: 'university_management_system'
+  };
