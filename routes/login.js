9d5c1d855f9d9ca4092b7e2c9880b02f163eb0ff
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2023-11-13 23:25:27 -0500

    Fixed formatting from formater misconfiguration between computers
    This issue should not pop up again
    
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- routes/login.js
+++ routes/login.js
@@ -5,31 +5,36 @@ import verify from "../data_validation.js";
 const router = Router();
 
 router.get("/", (req, res) => {
-	res.render("public/login");
+  res.render("public/login");
 });
 
 router.post("/", (req, res) => {
-	try {
-		const data = req.body;
-		const email = verify.email(data.email);
-		const password = verify.password(data.password);
-		const result = login(email, password);
+  try {
+    const data = req.body;
+    const email = verify.email(data.email);
+    const password = verify.password(data.password);
+    const result = login(email, password);
 
-		if (result.successful) {
-			req.session.id = result.id;
-			req.session.type = result.usertype;
-		}
+    if (result.successful) {
+      req.session.id = result.id;
+      req.session.type = result.usertype;
+    }
 
-		res.json({ loggedin: result.successful });
-	} catch (e) {
-		if (e === "Invalid email" || e === "Password is not a string" || e === "Password is empty" || e === "Password is too long") {
-			res.status(400);
-			res.json({ error: e });
-		} else {
-			res.status(500);
-			res.json("Login error");
-		}
-	}
+    res.json({ loggedin: result.successful });
+  } catch (e) {
+    if (
+      e === "Invalid email" ||
+      e === "Password is not a string" ||
+      e === "Password is empty" ||
+      e === "Password is too long"
+    ) {
+      res.status(400);
+      res.json({ error: e });
+    } else {
+      res.status(500);
+      res.json("Login error");
+    }
+  }
 });
 
 export default router;
