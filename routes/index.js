70ae275c732061af98ea00ec39f6faaad77dffee
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2023-11-16 19:11:00 -0500

    Made all pages meant for users private
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 
+import signin from "./middleware/signedin.js";
+
 import login from "./login.js";
 import logout from "./logout.js";
 import dashboard from "./dashboard.js";
@@ -9,6 +11,8 @@ function route(app) {
   app.use("/styles", express.static("./static/styles"));
 
   app.use("/login", login);
+
+  app.use("/", signin); // Only allow signed in users to access routes below this one
   app.use("/logout", logout);
   app.use("/dashboard", dashboard);
 }
