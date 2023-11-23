57a7f510a3e92edec90badde5737dc3b6dab351e
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2023-11-22 21:15:00 -0500

    Fixed bug in ssn formatter
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- static/scripts/createuser.js
+++ static/scripts/createuser.js
@@ -16,7 +16,19 @@ function formatssn() {
 
   location.value = ssn;
 }
+function lengthlimit() {
+  const location = document.getElementById("identification");
+  let ssn = location.value;
+
+  if (ssn.length - ssn.split("-").length + 1 > 9) {
+    ssn = ssn.substr(0, 9 + ssn.split("-").length - 1);
+    location.value = ssn;
+  }
+}
 
 document
   .getElementById("identification")
   .addEventListener("focusout", formatssn);
+document
+  .getElementById("identification")
+  .addEventListener("input", lengthlimit);
