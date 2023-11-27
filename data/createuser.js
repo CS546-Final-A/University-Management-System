96c64fb44a4b488f738766bdeed19431eab29053
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2023-11-26 21:29:32 -0500

    Set up form data parsing
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- data/createuser.js
+++ data/createuser.js
@@ -1,8 +1,7 @@
-import { hash } from "argon2";
 import { users } from "../config/mongoCollections.js";
 import verify from "../data_validation.js";
 
-async function createUser(type, email, password) {
+/*async function createUser(type, email, password) {
   // Quick mockup function for testing user login procedure. Incomplete and insecure. Do not use in current state
   email = verify.email(email);
   password = verify.password(password);
@@ -23,6 +22,56 @@ async function createUser(type, email, password) {
     type: type,
   });
   return results;
+}*/
+
+async function createUser(firstname, lastname, email, identification, type) {
+  firstname = verify.name(firstname);
+  lastname = verify.name(lastname);
+  email = verify.email(email);
+  if (identification.type === "ssn") {
+    identification.number = verify.ssn(identification.number);
+  } else {
+    throw "Invalid identification type";
+  }
+  type = verify.type(type);
+  const usercol = await users();
+
+  // Disallows multiple users with one email or identification number.
+  // Allows users with same identification number, but different account type as long as a different email is used.
+  const existinguser = await usercol.findOne(
+    {
+      $or: [
+        { email: email },
+        {
+          "identification.type": identification.type,
+          "identification.number": identification.number,
+          type: type,
+        },
+      ],
+    },
+    {
+      projection: {
+        _id: 0,
+        email: 1,
+      },
+    }
+  );
+  if (existinguser) {
+    if (existinguser.email === email) {
+      throw "A user with this email address already exists";
+    } else {
+      throw "A user with this id number already exists";
+    }
+  }
+
+  const userdata = {
+    firstname: firstname,
+    lastname: lastname,
+    email: email,
+    identification: identification,
+    type: type,
+    staus: "Initalized",
+  };
 }
 
 export default createUser;
--- routes/administration/users.js
+++ routes/administration/users.js
@@ -1,9 +1,32 @@
 import { Router } from "express";
 
+import verify from "../../data_validation.js";
+import createUser from "../../data/createuser.js";
+
 const router = Router();
 
 router.get("/create", (req, res) => {
   res.render("admin/createuser");
+});
+
+router.put("/create", (req, res) => {
+  try {
+    const firstname = verify.name(req.body.firstname);
+    const lastname = verify.name(req.body.lastname);
+    const email = verify.email(req.body.email);
+    const identification = req.body.identification;
+    if (identification.type === "ssn") {
+      identification.number = verify.ssn(identification.number);
+    } else {
+      throw "Invalid identification type";
+    }
+    const accountype = verify.accountype(req.body.accountype);
+
+    createUser(firstname, lastname, email, identification, accountype);
+  } catch (e) {
+    res.status(400);
+    res.send(e);
+  }
 });
 
 export default router;
