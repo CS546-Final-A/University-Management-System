41ab3917fb1fe4c97ac27a6a264a3b5e7a81ed6c
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2023-12-06 19:24:49 -0500

    Improved form data validation
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- data/identificationVerifierHTML.js
+++ data/identificationVerifierHTML.js
@@ -1,10 +1,10 @@
 function identificationVerificationHTML(type) {
   switch (type) {
     case "ssn":
-      return `<label for="id-confirmation">Please enter the last four digits of your social security number</label>
-            <input type="number" id="id-confirmation" name="idconf" placeholder="0000" maxlength="4">`;
+      return `<label for="id-confirmation">Please enter the last four digits of your social security number: </label>
+            <input type="text" id="id-confirmation" name="idconf" placeholder="0000" pattern="\\d{4}" maxlength="4">`;
     default:
-      throw { status: 400, message: "Invalid identification type" };
+      throw { status: 400, message: "Register" };
   }
 }
 
