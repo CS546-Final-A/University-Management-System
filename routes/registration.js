19f9b4bee413a98bd4a723dfbc47358a62e35a55
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2023-12-09 22:44:24 -0500

    Set up get route for password setting
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- routes/registration.js
+++ routes/registration.js
@@ -2,7 +2,8 @@ import { Router } from "express";
 
 import verify from "../data_validation.js";
 import identificationVerificationHTML from "../data/identificationVerifierHTML.js";
-import getIdentificationByUserID from "../data/getUnregisteredUserIdentification.js";
+import getUserByID from "../data/users/getUserInfoByID.js";
+import getUnregisteredIdentificationByUserID from "../data/getUnregisteredUserIdentification.js";
 
 function routeError(res, e) {
   if (e.status) {
@@ -23,13 +24,48 @@ function routeError(res, e) {
 
 const router = Router();
 
-router.get("/setpassword", (req, res) => {
-  if (!req.session.registrationuserid) {
-    const error = {
-      status: 401,
-      message: "You attempted to access a page without proper authentication",
-    };
-    return routeError(res, error);
+router.get("/setpassword", async (req, res) => {
+  try {
+    if (!req.session.registrationuserid) {
+      const error = {
+        status: 401,
+        message: "You attempted to access a page without proper authentication",
+      };
+      throw error;
+    }
+    let userinfo;
+    try {
+      userinfo = await getUserByID(req.session.registrationuserid, {
+        _id: 0,
+        firstname: 1,
+        lastname: 1,
+      });
+    } catch (e) {
+      // The userID has already been verified any issues resulting from the above code are caused by an error in the data function
+      // Set status to server error and rethrow
+      e.status = 500;
+      throw e;
+    }
+
+    return res.render("public/setpassword", userinfo);
+  } catch (e) {
+    return routeError(res, e);
+  }
+});
+
+router.patch("/setpassword", (req, res) => {
+  try {
+    if (!req.session.registrationuserid) {
+      const error = {
+        status: 401,
+        message: "You attempted to access a page without proper authentication",
+      };
+      throw error;
+    }
+
+    return res.render("public/setpassword");
+  } catch (e) {
+    return routeError(res, e);
   }
 });
 
@@ -40,7 +76,7 @@ router.get("/:userid", async (req, res) => {
   try {
     const id = req.params.userid;
 
-    const identification = await getIdentificationByUserID(id);
+    const identification = await getUnregisteredIdentificationByUserID(id);
 
     return res.render("public/registration", {
       identification: identification.type,
@@ -60,7 +96,7 @@ router.post("/:userid", async (req, res) => {
   let identification;
   try {
     const id = req.params.userid;
-    identification = await getIdentificationByUserID(id);
+    identification = await getUnregisteredIdentificationByUserID(id);
   } catch (e) {
     return routeError(res, e);
   }
