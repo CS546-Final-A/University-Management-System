f4c7a2a61e78c581548bcf55fa0bde6ad114adba
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2023-12-09 22:49:28 -0500

    Set form input to required for social security numbers
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- data/identificationVerifierHTML.js
+++ data/identificationVerifierHTML.js
@@ -2,7 +2,7 @@ function identificationVerificationHTML(type) {
   switch (type) {
     case "ssn":
       return `<label for="id-confirmation">Please enter the last four digits of your social security number: </label>
-            <input type="text" id="id-confirmation" name="idconf" placeholder="0000" pattern="\\d{4}" maxlength="4">`;
+            <input type="text" id="id-confirmation" name="idconf" placeholder="0000" pattern="\\d{4}" maxlength="4" required>`;
     default:
       throw { status: 400, message: "Register" };
   }
