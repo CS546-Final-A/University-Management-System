d8acade79124b40b844f6cce159f3d12bbb6f92b
kushaltrivedi5 <40542375+kushaltrivedi5@users.noreply.github.com>
2023-12-11 03:50:46 -0500

    Navbar modifications
    Added UserIcon, modified session variable

--- routes/login.js
+++ routes/login.js
@@ -19,6 +19,8 @@ router.post("/", async (req, res) => {
     if (result.successful) {
       req.session.userid = result.id;
       req.session.type = result.type;
+      req.session.name = result.name;
+      req.session.email = result.email;
     }
 
     res.json({ loggedin: result.successful });
