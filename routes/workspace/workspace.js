3c6500a4d29ad8fc2254451727ec6728a262d180
Shikhar Saxena <saxenashikhar01@gmail.com>
2023-12-11 06:10:51 -0500

    added workspace and modules

---
+++ routes/workspace/workspace.js
@@ -0,0 +1,49 @@
+import { Router } from "express";
+const router = Router();
+import getSectionById from "../../data/sections/sections.js";
+import { getCourseById } from "../../data/courses/courses.js";
+import getUserByID from "../../data/users/getUserInfoByID.js";
+router.route("/:sectionId").get(async (req, res) => {
+  const section = await getSectionById(req.params.sectionId);
+  const course = await getCourseById(section.courseId.toString());
+
+  const profName = await getUserByID(section.sectionInstructor, {
+    _id: 0,
+    firstname: 1,
+    lastname: 1,
+  });
+
+  res.render("workspace/section", {
+    sectionID: `${section._id}`,
+    courseId: `${section.courseId.toString()}`,
+    courseName: `${course.courseName}`,
+    sectionName: `${section.sectionName}`,
+    sectionInstructor: `${section.sectionInstructor}`,
+    fN: `${profName.firstname}`,
+    lN: `${profName.lastname}`,
+    sectionType: `${section.sectionType}`,
+    sectionStartTime: `${section.sectionStartTime}`,
+    sectionEndTime: `${section.sectionEndTime}`,
+    sectionDay: `${section.sectionDay}`,
+    sectionCapacity: `${section.sectionCapacity}`,
+    sectionYear: `${section.sectionYear}`,
+    sectionSemester: `${section.sectionSemester}`,
+    students: `${section.students}`,
+    sectionLocation: `${section.sectionLocation}`,
+    sectionDescription: `${section.sectionDescription}`,
+  });
+});
+router.route("/:sectionId/modules").get(async (req, res) => {
+  const section = await getSectionById(req.params.sectionId);
+  res.render("workspace/module", {
+    modules: section.sectionModules,
+    sectionID: `${section._id}`,
+  });
+});
+
+router.route("/:sectionId/modules/attendance").get(async (req, res) => {
+  console.log("Attendance");
+  res.render("workspace/attendance");
+});
+
+export default router;
---
+++ views/workspace/attendance.handlebars
@@ -0,0 +1,1 @@
+<h1>Attendance</h1>
