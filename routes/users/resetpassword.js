cc40636e65cbdd99a8d280529e806fad68ca134e
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2023-12-11 19:52:29 -0500

    Load javascript file using a partial
    Enables use of jquery
    
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- routes/users/resetpassword.js
+++ routes/users/resetpassword.js
@@ -5,7 +5,9 @@ import verify from "../../data_validation.js";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.render("public/requestpasswordreset");
+  res.render("public/requestpasswordreset", {
+    script: "requestpasswordreset",
+  });
 });
 
 export default router;
