6759775f61813fe58d18d5f74a759584faebebca
Prasanna <limayeprasanna15@gmail.com>
2023-12-11 23:19:23 -0500

    Course Enrollment Index page, Listing Page with filter and redirect to course implemented

--- routes/courses/courses.js
+++ routes/courses/courses.js
@@ -1,35 +1,71 @@
-import { Router } from "express";
+import { Router, query } from "express";
 import { santizeInputs } from "../../data_validation.js";
 import * as courseDataFunctions from "../../data/courses/courses.js";
 import util from "util";
 import { unsubscribe } from "diagnostics_channel";
 
 const router = Router();
+router.get("/", async (req, res) => {
+  let uniqueSectionYearandSemester =
+    await courseDataFunctions.getUniqueSectionYearandSemester();
+  // console.log(
+  //   util.inspect(
+  //     uniqueSectionYearandSemester,
+  //     false,
+  //     null,
+  //     true /* enable colors */
+  //   )
+  // );
+  console.log("yes");
+  res.render("courses/index", {
+    uniqueYear: uniqueSectionYearandSemester[0],
+    uniqueSemester: uniqueSectionYearandSemester[1],
+  });
+});
 
-router.get("/listings", async (req, res) => {
-  console.log("params is");
-  console.log(req.query);
+router.get("/:year/:semester/listings", async (req, res) => {
+  const { year, semester } = req.params;
 
-  let data = await courseDataFunctions.getAllCourses();
-  // console.log(util.inspect(data, false, null, true /* enable colors */));
-
-  // Function to get unique values from an array
-  const getUniqueValues = (array) => [...new Set(array.flat(Infinity))];
-  data = data.filter((course) => course.sectionId.length > 0);
-  const uniqueDepartmentNames = getUniqueValues(
-    data.map((course) => course.departmentName.toString())
+  console.log(year, semester);
+  let {
+    searchTerm,
+    departmentFilter,
+    instructorFilter,
+    meetingDaysFilter,
+    deliveryModeFilter,
+  } = req.query;
+  let data = await courseDataFunctions.getAllCourses(
+    year,
+    semester,
+    searchTerm,
+    departmentFilter,
+    instructorFilter,
+    meetingDaysFilter,
+    deliveryModeFilter
   );
-  const uniqueDays = getUniqueValues(data.map((course) => course.sectionDay));
-  const uniqueSectionTypes = getUniqueValues(
-    data.map((course) => course.sectionType)
-  );
+  data.year = year;
+  data.semester = semester;
 
-  data.uniqueDays = uniqueDays;
+  let uniqueDepartmentNames =
+    await courseDataFunctions.getUniqueDepartmentNamesandId();
+  let uniqueInstructors =
+    await courseDataFunctions.getUniqueInstructorNamesandId();
+
+  // // console.log(department);
+  // console.log(instructors);
+
   data.uniqueDepartmentNames = uniqueDepartmentNames;
+  data.uniqueInstructors = uniqueInstructors;
+
+  let uniqueSectionTypes = ["Online", "In-Person"];
   data.uniqueSectionTypes = uniqueSectionTypes;
-  console.log("data is");
-  console.log(data);
-  res.render("courses/listings", { courses: data });
+  data.map((course) => {
+    course.departmentName = course.departmentName[0];
+  });
+  // console.log(util.inspect(data, false, null, true /* enable colors */));
+  res.render("courses/listings", {
+    courses: data,
+  });
 });
 
 export default router;
