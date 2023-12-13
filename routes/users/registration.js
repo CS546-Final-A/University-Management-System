ee7ed8f9be505961481d5ea69ae33a45a929f7b3
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2023-12-13 16:53:06 -0500

    Moved error routing to its own function
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- routes/users/registration.js
+++ routes/users/registration.js
@@ -4,23 +4,7 @@ import verify from "../../data_validation.js";
 import getUserByID from "../../data/users/getUserInfoByID.js";
 import getUnregisteredIdentificationByUserID from "../../data/users/getUnregisteredUserIdentification.js";
 import setPassword from "../../data/users/setPasswordByID.js";
-
-function routeError(res, e) {
-  if (e.status) {
-    res.status(e.status);
-    if (e.status >= 500) {
-      console.log("Error:");
-      console.log(e.message);
-      e.message = "Internal server error";
-    }
-    return res.render("public/error", { error: e.message });
-  } else {
-    console.log("Error:");
-    console.log(e);
-    res.status(500);
-    return res.render("public/error", { error: "Internal server error" });
-  }
-}
+import routeError from "../routeerror.js";
 
 const router = Router();
 
