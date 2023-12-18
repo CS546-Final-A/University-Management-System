4f6f52f0f4d32a8729921c2c12d405c052bc2ff5
Prasanna <limayeprasanna15@gmail.com>
2023-12-17 21:18:45 -0500

    Added logic for seeding

--- seed/importData.js
+++ seed/importData.js
@@ -1,8 +1,14 @@
 // importData.js
 
 import { MongoClient } from "mongodb";
-import seedData from "./seedData.json";
+import courseData from "./courseData.json";
+import userData from "./userData.json";
+import departmentData from "./departmentData.json";
+import sessionData from "./sessionData.json";
+import assignmentData from "./assignmentData.json";
+import path from "path";
 
+const fs = require("fs");
 const importData = async () => {
   const uri = "mongodb://localhost:27017/your-database-name";
   const client = new MongoClient(uri, {
@@ -12,15 +18,34 @@ const importData = async () => {
 
   try {
     await client.connect();
+    const collections = [
+      "courses",
+      "users",
+      "departments",
+      "sessions",
+      "assignments",
+    ];
+    const loadJSON = (filePath) => {
+      try {
+        const jsonData = fs.readFileSync(filePath, "utf8");
+        return JSON.parse(jsonData);
+      } catch (error) {
+        console.error(`Error loading JSON file: ${error}`);
+        return null;
+      }
+    };
 
-    const database = client.db();
-    const collection = database.collection("");
-
-    await collection.deleteMany({});
-
-    await collection.insertMany(seedData);
-
-    console.log("Data imported successfully from seedData.json");
+    collections.forEach(async (collectionName) => {
+      const database = client.db();
+      const collection = database.collection(collectionName);
+      const jsonData = loadJSON("./seeds/" + collectionName + "Data.json");
+      await collection.deleteMany({});
+      const x = await collection.insertMany(jsonData);
+      console.log(x);
+      console.log(
+        "Data imported successfully from " + collectionName + "Data.json"
+      );
+    });
   } finally {
     await client.close();
   }
